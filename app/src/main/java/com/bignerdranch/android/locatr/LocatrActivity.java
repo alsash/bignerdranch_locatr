4c8a01af0fec1c7bc6e276fde42efdafdd44aeee
alsash <alsashcom@gmail.com>
2016-11-16 14:21:39 +0200

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.locatr;
+
+import android.support.v4.app.Fragment;
+
+public class LocatrActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
